f4df83bc24b7858d832ef5d99b1d7cff35467b44
NAEL2XD <108278470+NAEL2XD@users.noreply.github.com>
2025-09-07 16:32:10 +0200

    use ARGB instead of RGBA

--- citro/util/CitroObjectUtil.hx
+++ citro/util/CitroObjectUtil.hx
@@ -10,6 +10,7 @@ import citro.object.CitroObject;
 @:cppFileCode('
 #include <citro2d.h>
 #include "citro_CitroInit.h"
+#include "citro_object_CitroSprite.h"
 ')
 
 class CitroObjectUtil {
@@ -35,22 +36,22 @@ class CitroObjectUtil {
      * Draws a triangular gradient with arguments specified.
      * @param arrayX An array of the X's coordinate of this vertex of this triangle, must be 3 or more or it'll return false.
      * @param arrayY An array of the Y's coordinate of this vertex of this triangle, must be 3 or more or it'll return false.
-     * @param arrayC An array of 32-bit RGBA color of the vertex of the triangle.
+     * @param arrayC An array of 32-bit ARGB color of the vertex of the triangle. `0: Bottom Left, 1: Top Middle, 2: Bottom Right`. If len is 1 then it will be a solid color.
      * @param drawBottom Whetever or not it should draw in the bottom screen.
      * @return `true` if successfully drawn, `false` if one of the arrays is not greater than 2
      */
     public static function drawTriangle(arrayX:Array<Float>, arrayY:Array<Float>, arrayC:Array<UInt32>, drawBottom:Bool = false):Bool {
         if (arrayX.length > 2 && arrayY.length > 2 && arrayC.length > 0) {
-            while (arrayC.length != 3) {
+            while (arrayC.length < 3) {
                 arrayC.push(arrayC[0]);
             }
 
             untyped __cpp__('
                 C2D_SceneBegin(drawBottom ? bottomScreen : topScreen);
                 C2D_DrawTriangle(
-                    (*arrayX)[0], (*arrayY)[0], (*arrayC)[0],
-                    (*arrayX)[1], (*arrayY)[1], (*arrayC)[1],
-                    (*arrayX)[2], (*arrayY)[2], (*arrayC)[2],
+                    (*arrayX)[0], (*arrayY)[0], colorConvert((*arrayC)[0]),
+                    (*arrayX)[1], (*arrayY)[1], colorConvert((*arrayC)[1]),
+                    (*arrayX)[2], (*arrayY)[2], colorConvert((*arrayC)[2]),
                     1
                 )
             ');
@@ -63,7 +64,7 @@ class CitroObjectUtil {
      * Draws a linely gradient with arguments specified
      * @param arrayX An array of the X's Horizontal position of the vertex of this line, must be 2 or more or it'll return false.
      * @param arrayY An array of the Y's Vertical position of the vertex of this line, must be 2 or more or it'll return false.
-     * @param arrayC An array of the Color's 32-bit RGBA color of the vertex of this line.
+     * @param arrayC An array of the Color's 32-bit ARGB color of the vertex of this line. `0: Left, 1: Right`. If len is 1 then it will be a solid color.
      * @param thickness Thickness, in pixels, of the line
      * @param drawBottom Whetever or not it should draw in the bottom screen.
      * @return `true` if successfully drawn, `false` if one of the arrays is not greater than 1.
@@ -77,8 +78,8 @@ class CitroObjectUtil {
             untyped __cpp__('
                 C2D_SceneBegin(drawBottom ? bottomScreen : topScreen);
                 C2D_DrawLine(
-                    (*arrayX)[0], (*arrayY)[0], (*arrayC)[0],
-                    (*arrayX)[1], (*arrayY)[1], (*arrayC)[1],
+                    (*arrayX)[0], (*arrayY)[0], colorConvert((*arrayC)[0]),
+                    (*arrayX)[1], (*arrayY)[1], colorConvert((*arrayC)[1]),
                     thickness, 1
                 )
             ');
@@ -93,17 +94,22 @@ class CitroObjectUtil {
      * @param y The Y's Position to use.
      * @param w The Width for the rectangle.
      * @param h The Height for the rectangle.
-     * @param color An array of colors, if color array has 3> length then uses gradient, else uses solic color.
+     * @param color An array of colors, if color array has 3> length then uses gradient, else uses solic color. `0: Top Left, 1: Top Right, 2: Bottom Left, 3: Bottom Right.`
      * @param drawBottom Whetever or not it should draw in the bottom screen.
      * @return `true` if length is not equal than 0
      */
     public static function drawRect(x:Float, y:Float, w:Float, h:Float, color:Array<UInt32>, drawBottom:Bool = false):Bool {
-        untyped __cpp__('C2D_SceneBegin(drawBottom ? bottomScreen : topScreen)');
         return color.length > 1 ? {
-            while (color.length != 4) {
+            while (color.length < 4) {
                 color.push(color[0]);
             }
-            untyped __cpp__('C2D_DrawRectangle(x, y, 1, w, h, (*color)[0], (*color)[1], (*color)[2], (*color)[3])');
+            untyped __cpp__('
+                C2D_SceneBegin(drawBottom ? bottomScreen : topScreen);
+                C2D_DrawRectangle(
+                    x, y, 1, w, h, colorConvert((*color)[0]), colorConvert((*color)[1]),
+                    colorConvert((*color)[2]), colorConvert((*color)[3])
+                )
+            ');
             true;
         } : false;
     }
